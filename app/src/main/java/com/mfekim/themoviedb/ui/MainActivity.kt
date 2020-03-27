a7bdcd5195441fa951cc73a53cfd33f67ac549ec
mfekim <mfekim@gmail.com>
2020-03-27 15:29:37 +0100

    init Android Studio project

---
+++ app/src/main/java/com/mfekim/themoviedb/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mfekim.themoviedb.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mfekim.themoviedb.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
